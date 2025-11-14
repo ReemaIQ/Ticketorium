1f12de239e12697234936ac4905a76d2b51248ac
AK4-Arfaj <ak4.arfaj@gmail.com>
2025-11-14 20:11:14 +0300

    Added responsiveness to Visitor/Student homepage, changed the style of not-yet-functioning search and filter buttons

--- ticketorium/src/pages/user_home/UserHome.jsx
+++ ticketorium/src/pages/user_home/UserHome.jsx
@@ -1,6 +1,16 @@
 import EventList from "../../components/event-list/EventList.jsx";
-import React from "react";
-import {Hash, Search} from "lucide-react";
+import {useState} from "react";
+// Font Awesome Setup
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import SearchBtn from "../../components/SearchBtn/SearchBtn.jsx";
+
+
+library.add(fas, far, fab)
 
 const contentOptions = {
     // student, visitor, organizer, admin, system-admin
@@ -83,56 +93,46 @@ const contentOptions = {
 function UserHome(props) {
     return (
         <>
-        <div className="m-0 text-3xl flex flex-col gap-10 bg-[var(--secondary-color)] w-full h-screen relative">
-            <div className="flex justify-between">
-                <div className="px-20 py-30">
-                    <h1 className="text-[130px] font-bold w-180 font-[Epilogue-Black] leading-[140px] text-white">Welcome Back, {props.users[props.user]["first-name"]}!</h1>
-                    <p className="font-[DM-Sans-Light] text-[24px] text-white mt-7">All {(props.users[props.user]["type"] === "visitor" || props.users[props.user]["type"] === "system-admin")? "this": "your"} university's events in one place.</p>
+        <div className=" m-0 py-10 text-3xl flex flex-col bg-[var(--secondary-color)] w-full relative">
+            <div className="flex justify-between order-2 xl:order-1">
+                <div className="xl:px-20 xl:py-30 px-10 py-10 sm:max-xl:flex sm:max-xl:flex-col sm:max-xl:w-full">
+                    <h1 className="flex flex-col items-center max-xl:text-center text-[86px] sm:text-[110px] md:text-[130px] font-bold font-[Epilogue-Black] md:leading-[125px] text-white">Welcome<br/>Back,<br/>{props.users[props.user]["first-name"]}!</h1>
+                    <p className="max-xl:flex flex-col items-center font-[DM-Sans-Light] text-[24px] text-white mt-7">All {(props.users[props.user]["type"] === "visitor" || props.users[props.user]["type"] === "system-admin")? "this": "your"} university's events in one place.</p>
                 </div>
             </div>
 
-            <img src={"/src/assets/images/home-main/unis/" + props.universities[props.users[props.user]["university"]]["logo"]} className="h-[95%] absolute right-[-25%]"/>
+            <img src={"/src/assets/images/home-main/unis/" + props.universities[props.users[props.user]["university"]]["logo"]} className="max-md:w-[40%] md:max-lg:w-[30%] lg:max-xl:w-[25%] xl:h-[85%] 2xl:h-[95%] xl:top-[7%] xl:right-[-8%] 2xl:top-[2%] xl:absolute order-1 xl:order-2 self-center"/>
         </div>
 
         {/* Jump-to section */}
-        <div className="bg-[#F3F3F3] h-[96px] w-full flex items-center px-12 py-9 gap-12">
-            <span className="font-[Gilroy-Black] text-[var(--secondary-color)] text-[32px]">JUMP TO</span>
-            <div className="flex justify-between w-[55%]">
+        <div className="bg-[#F3F3F3] xl:h-[96px] w-full xl:flex xl:flex-row xl:items-center px-12 py-9 gap-12">
+            <div className="font-[Gilroy-Black] text-[var(--secondary-color)] text-[32px] mb-10 xl:mb-0 shrink-0">JUMP TO</div>
+            <div className="flex max-xl:justify-between max-xl:grid max-xl:grid-cols-2 gap-25 xl:gap-15 w-full">
                 {Object.keys(contentOptions[props.users[props.user]["type"]]).map((key) =>
-                    <span className="font-[Gilroy-Medium] text-[20px] text-[var(--primary-color)]" onClick={() => {window.scrollTo({top: document.getElementById(key).offsetTop - 30, behavior:'smooth'})}}>{contentOptions[props.users[props.user]["type"]][key]["jump-to"]}</span>
+                    <span className="font-[Gilroy-Medium] text-[20px] text-[var(--primary-color)] self-center text-center cursor-pointer" onClick={() => {window.scrollTo({top: document.getElementById(key).offsetTop - 30, behavior:'smooth'})}}>{contentOptions[props.users[props.user]["type"]][key]["jump-to"]}</span>
                 )}
             </div>
         </div>
 
         {/* Sections */}
-        <div className="flex flex-col items-center py-10 px-15 gap-10">
+        <div className="flex flex-col items-center xl:py-10 px-10 xl:px-15 gap-5 w-full">
             {Object.keys(contentOptions[props.users[props.user]["type"]]).map(key => 
                 <>
-                <div id="section-header" className="flex items-center justify-between w-full mt-9 mb-3 px-15">
+                <div id="section-header" className="flex items-center justify-between w-full mt-9 mb-3">
                     {/* Left: Title + Search */}
-                    <div className="flex items-center gap-3">
-                        <h2 id={key} className="font-[Epilogue-Black] text-[60px] text-[var(--primary-color)]">{contentOptions[props.users[props.user]["type"]][key]["header"]}</h2>
-
-                        {/* Search Button */}
-                        {/*onClick={onSearch}*/}
-                        <button
-                            className="w-10 h-10 flex items-center justify-center rounded-full border border-gray-200 shadow-sm hover:bg-gray-50 transition"
-                            aria-label="Search"
-                        >
-                            <Search size={20} className="text-gray-500" />
-                        </button>
+                    <div className="flex flex-col items-start gap-4 w-full">
+                        <h2 id={key} className="font-[Epilogue-Black] text-[50px] xl:text-[60px] text-[var(--primary-color)]">{contentOptions[props.users[props.user]["type"]][key]["header"]}</h2>
+                        <div className="flex gap-4 self-start w-full justify-center">
+                            <button className="p-2 rounded-full bg-purple-700 w-12 h-12 cursor-pointer hover:ring-4 ring-[rgba(0,0,0,0.1)] shrink-0">
+                                <FontAwesomeIcon
+                                icon={"fa-solid fa-filter"}
+                                className="text-white"
+                                />
+                            </button>
+                            <SearchBtn searchingFor={key}/>
+                        </div>
                     </div>
-
-                    {/* Right: Filter Button */}
-                    {/*onClick={onFilter}*/}
-                    <button
-                        className="flex items-center gap-2 border-2 border-[#4F6FFF]
-                        text-[#14113B] px-5 py-2 rounded-full font-[Gilroy-Medium]
-                        hover:bg-[#4F6FFF] hover:text-white transition"
-                    >
-                        <Hash size={18} />
-                        Filter
-                    </button>
+                    
                 </div>
 
                 <div className="flex w-full max-w-6xl">
