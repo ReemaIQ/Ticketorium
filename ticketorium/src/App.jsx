a4150ee3efff15d823dd9401c9f633a56e9ab258
ReemaIQ <s202244660@kfupm.edu.sa>
2025-11-15 21:33:05 +0300

    Organizer Home Page with Analytics

--- ticketorium/src/App.jsx
+++ ticketorium/src/App.jsx
@@ -8,6 +8,7 @@ import UserHome from './pages/user_home/UserHome.jsx'
 import AllEvents from "./pages/AllEvents.jsx";
 import MyEvents from "./pages/MyEvents.jsx";
 import EventPage from "./pages/event_details/event_page.jsx";
+import OrganizerHomePage from "./pages/home/Organizer.jsx" //r
 
 // fyi, all uses of localstorage will be db later EXCEPT for loggedInUser
 
@@ -269,7 +270,25 @@ function App() {
     <>
       <Nav type={loggedInUser? dummyUsers.current[loggedInUser]["type"]: "empty"} userName={loggedInUser? dummyUsers.current[loggedInUser]["first-name"]: ""} setLoggedInUser={setLoggedInUser}/>
       <Routes>
-        <Route path="/home" element={!loggedInUser? <DummyUserHome/>: <UserHome user={loggedInUser} users={dummyUsers.current} universities={dummyUniversities.current} events={dummyEvents.current}/>}/> {/* main home page for not logged in users */}
+
+          <Route path="/home"
+              element={
+                  !loggedInUser ? (
+                      <DummyUserHome /> //reema: not logged in to dummy landing home
+                  ) : dummyUsers.current[loggedInUser]["type"] === "organizer" ? (
+                      <OrganizerHomePage /> //reema: organizer page
+                  ) : (
+                      <UserHome
+                          user={loggedInUser}
+                          users={dummyUsers.current}
+                          universities={dummyUniversities.current}
+                          events={dummyEvents.current}
+                      />
+                  )
+              }
+          />
+
+          {/*<Route path="/home" element={!loggedInUser? <DummyUserHome/>:  <UserHome user={loggedInUser} users={dummyUsers.current} universities={dummyUniversities.current} events={dummyEvents.current}/>}/> /!* main home page for not logged in users *!/*/}
         {/*<Route path="/visitor/home" element={!loggedInUser? <DummyUserHome/> : dummyUsers.current[loggedInUser]["type"] != "visitor"? <Navigate to={`/${dummyUsers.current[loggedInUser]["type"]}/home`}/>: <UserHome user={loggedInUser} users={dummyUsers.current} universities={dummyUniversities.current} events={dummyEvents.current}/>}/>*/}
         {/*<Route path="/student/home" element={!loggedInUser? <DummyUserHome/> : dummyUsers.current[loggedInUser]["type"] != "student"? <Navigate to={`/${dummyUsers.current[loggedInUser]["type"]}/home`}/>: <UserHome user={loggedInUser} users={dummyUsers.current} universities={dummyUniversities.current} events={dummyEvents.current}/>}/>*/}
         <Route path="/log-in" element={loggedInUser? <Navigate to={`/home`}/> : <SignupLogin option={"log-in"} checkIfEmailExists={checkIfEmailExists} checkIfUsernameExists={checkIfUsernameExists} checkUsernamePassword={checkUsernamePassword} checkEmailPassword={checkEmailPassword} setLoggedInUser={setLoggedInUser} getUsernameFromEmail={getUsernameFromEmail}/>}/>
